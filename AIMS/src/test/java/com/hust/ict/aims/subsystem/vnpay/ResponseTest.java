0e67b51ca0e64b56e59fbc0572199c044f56ed97
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-05-24 23:52:28 +0800

    chore: add junit test and reduced java version to 17

---
+++ AIMS/src/test/java/com/hust/ict/aims/subsystem/vnpay/ResponseTest.java
@@ -0,0 +1,13 @@
+package com.hust.ict.aims.subsystem.vnpay;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ResponseTest {
+	@Test
+	void givenEvenNumber_whenCheckingIsNumberEven_thenTrue() {
+	    boolean result = true;
+
+	    Assertions.assertTrue(result);
+	}
+}

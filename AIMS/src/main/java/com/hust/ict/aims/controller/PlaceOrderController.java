7e1c30da4c5c14c251d4ffaa44a1f041837fed6c
NguyenChanHung1 <100984506+NguyenChanHung1@users.noreply.github.com>
2024-05-26 21:28:37 +0700

    Feat: add Place order controller

---
+++ AIMS/src/main/java/com/hust/ict/aims/controller/PlaceOrderController.java
@@ -0,0 +1,58 @@
+package com.hust.ict.aims.controller;
+
+import com.hust.ict.aims.entity.cart.Cart;
+import com.hust.ict.aims.entity.cart.CartMedia;
+import com.hust.ict.aims.entity.invoice.Invoice;
+import com.hust.ict.aims.entity.order.Order;
+import com.hust.ict.aims.entity.order.OrderMedia;
+import com.hust.ict.aims.entity.shipping.DeliveryInfo;
+import com.hust.ict.aims.exception.placement.InsufficientProductException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import com.hust.ict.aims.service.CartService;
+
+import java.util.List;
+
+public class PlaceOrderController extends BaseController{
+    private CartService cartService;
+    private Cart cart;
+    private DeliveryInfo deliveryInfo;
+    private Order regularOrder;
+    private Order rushOrder;
+
+    public PlaceOrderController() {
+        this.cartService = new CartService();
+    }
+
+    public void placeOrder() {
+        try{
+            cartService.checkAvailabilityOfProduct();
+        }
+        catch(Exception e) {
+            // Display alert
+            e.printStackTrace();
+        }
+    }
+
+    public void placeRushOrder() {
+
+    }
+
+    public Order createOrder(DeliveryInfo deliveryInfo) {
+        this.deliveryInfo = deliveryInfo;
+        Order order = new Order();
+        order.setDeliveryInfo(deliveryInfo);
+        for(Object obj : cartService.getListMedia()) {
+            CartMedia cartMedia = (CartMedia) obj;
+            OrderMedia orderMedia = new OrderMedia(cartMedia.getMedia(), cartMedia.getQuantity(),
+                    cartMedia.getPrice());
+            order.getLstOrderMedia().add(orderMedia);
+        }
+        return order;
+    }
+
+    public Invoice createInvoice(Order order) {
+        return new Invoice(order);
+    }
+}
